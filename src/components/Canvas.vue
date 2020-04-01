a8743255eb5bc9e5369ec425ccfcd1b1b9330218
rurusyu <Charizard006@protonmail.com>
2020-04-02 03:12:54 +0900

    first commit;

---
+++ src/components/Canvas.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <canvas ref="can" width="400" height="400" id="canvas1"></canvas>
+  </div>
+</template>
+
+<script>
+
+import { fabric } from 'fabric';
+
+export default {
+  name: 'Canvas',
+ 
+  data(){
+    return {
+      canvas:'',
+      ref:'',
+      rect:0,
+      triangle:0,
+      circle:0,
+    }
+  },
+  props:{
+    shapeList: {
+      type: Array,
+      required: false,
+    }
+  },
+   watch:{
+    shapeList(){
+      console.log('watch',this.$props.shapeList[1]);
+    
+    }
+  },
+  created(){
+    this.rect = this.$props.shapeList[0].rect;
+    this.triangle = this.$props.shapeList[1].triangle;
+    this.circle = this.$props.shapeList[2].circle;
+    console.log(this.rect,this.triangle,this.circle);
+  },
+  mounted(){
+    this.ref = this.$refs.can;
+    this.canvas = new fabric.Canvas(this.ref);
+    console.log('mounted',this.$props.shapeList[0].rect);
+  },
+ 
+
+  methods:{
+    addRect(){
+      let rect = new fabric.Rect({
+         left: 10,
+         top: 10,
+         height: 50,
+         width: 50,
+         fill: 'green',
+         stroke:'black'
+      })
+      this.canvas.add(rect);
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+#canvas1 {
+  border : 1px solid red;
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,78 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="item in num" :key="item">
+      <canvas-view :shapeList="shapeList"></canvas-view>
+    </div>
+    <button @click="addCanvas">+</button>
+    <div class="menu">
+      <div class="rect" @click="clickRect">사각형</div>
+      <div class="rect" @click="clickTriAngle">삼각형</div>
+      <div class="rect" @click="clickCircle">원형</div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CanvasView from '@/components/Canvas.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    CanvasView
+  },
+  data(){
+    return {
+      num: 1,
+      shapeList: [
+        {rect:0},
+        {triangle:0},
+        {circle:0},
+      ]
+    }
+  },
+  methods:{
+    addCanvas(){
+      this.num++;
+      console.log('num', this.num);
+    },
+    clickRect(){
+      console.log('클릭');
+      // this.shapeList.push('rect')
+      this.shapeList[0].rect++;
+      this.$set(this.shapeList);
+      
+    },
+    clickTriAngle(){
+      console.log('클릭');
+      // this.shapeList.push('rect')
+      this.shapeList[1].triangle++;
+      this.$set(this.shapeList);
+    },
+    clickCircle(){
+      console.log('클릭');
+      // this.shapeList.push('rect')
+      this.shapeList[2].circle++;
+      this.$set(this.shapeList);
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+.home{
+    border: 1px solid blue;
+
+  .menu {
+    border: 1px solid blue;
+    width: 200px;
+    height: 300px;
+
+    .rect{
+      border: 1px solid red;
+      width: 200px;
+      height: 50px;
+
+    }
+  }
+}
+</style>
